bd87408b966ccd1c70a4716e92029ce260b6f0c6
ANIL P <anilkumar369cec@gmail.com>
2023-05-26 09:02:30 +0530

    message

--- src/components/date/Date.js
+++ src/components/date/Date.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import './Date.css'
-import Couple from '../../assets/images/couple.png'
+import Couple from '../../assets/images/couple.jpg'
 import DateImage from '../../assets/images/dateImage.jpg'
 
 function Date() {
   return (
-    <div className='date' style={{backgroundImage: `url("${DateImage}")`}}>
-      <div className='date_image' style={{backgroundImage: `url("${Couple}")`}}>
+    <div className='date' style={{backgroundImage: `url("${Couple}")`}}>
         <div className='date_text'>
           <h3>We Are Getting Married</h3>
           <h1>Save the date</h1>
           <p>07 June 2023</p>
         </div>
-      </div>
     </div>
   )
 }
